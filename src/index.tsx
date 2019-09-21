8fdfcfe8365af1a45f29c121d1fa3a150ac0e3e8
zhangjun1 <zhangjun1@onething.net>
2019-09-21 13:33:01 +0800

    webpack升级

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-// import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import App from './app';
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// registerServiceWorker();
+serviceWorker.unregister();
