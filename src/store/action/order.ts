9de81762d7757d1f4a715d903393e09bd033a8c1
zhangjun1 <zhangjun1@onething.net>
2019-10-07 10:37:08 +0800

    getstate

--- src/store/action/order.ts
+++ src/store/action/order.ts
@@ -1,7 +1,9 @@
 import { Dispatch } from 'redux';
 import { SAVE_ORDER } from '@/constants/actionTypes';
-export const saveOrderId = (params: any) => async (dispatch: Dispatch) => {
+export const saveOrderId = (params: any) => async (dispatch: Dispatch,getSate:any) => {
   try {
+    const order=getSate();
+    console.log(order);
     dispatch({
       type: SAVE_ORDER,
       payload: params
